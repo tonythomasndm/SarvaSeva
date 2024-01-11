06e2f1918d2e6b1681408386be878cda238ae923
Arpan Kumar <108618629+arpan21020@users.noreply.github.com>
2024-01-11 15:50:54 +0000

    Commit working

--- Components/Admin/AdminPage.js
+++ Components/Admin/AdminPage.js
@@ -11,7 +11,7 @@ export const AdminPage=(props)=>{
   return(
     <View style={styles.container}>
         <TouchableOpacity onPress={() => props.navigation.navigate('CreateEvent')} style={styles.button}>
-      <Text style={styles.buttons}>Create Eventss</Text>
+      <Text style={styles.buttons}>Create Eventsssss</Text>
       </TouchableOpacity>
       <TouchableOpacity onPress={() => props.navigation.navigate('ApprovedEvents')} style={styles.button}>
       <Text style={styles.buttons}>Approved Events</Text>
